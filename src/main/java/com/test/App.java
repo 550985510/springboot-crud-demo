7fa0825599cf56b69ab4280adf2d847ff5c95663
baby <550985510@163.com>
2017-11-17 19:56:20 +0800

    version 1.0.1

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -23,22 +23,6 @@ public class App extends SpringBootServletInitializer {
         return application.sources(App.class);
     }
 
-<<<<<<< HEAD
-=======
-    //配置mybatis的分页插件pageHelper
-      @Bean
-      public PageHelper pageHelper(){
-                 PageHelper pageHelper = new PageHelper();
-                 Properties properties = new Properties();
-                 properties.setProperty("offsetAsPageNum","true");
-                 properties.setProperty("rowBoundsWithCount","true");
-                 properties.setProperty("reasonable","false");
-                 properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
-                 pageHelper.setProperties(properties);
-                 return pageHelper;
-             }
-
->>>>>>> 81de896644044df77f117147d612c816d813c13c
     public static void main(String[] args){
         SpringApplication.run(com.test.App.class,args);
     }
