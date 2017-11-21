23fc69bd6254d0888ec2687bd13293bff028e2dc
baby <550985510@163.com>
2017-11-21 20:01:20 +0800

    Vue版本新增邮箱格式校验以及删除、批量删除功能

--- src/main/java/com/test/controller/UserController.java
+++ src/main/java/com/test/controller/UserController.java
@@ -7,19 +7,13 @@ import com.test.bean.Users;
 import com.test.service.UsersService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 
 @RestController
